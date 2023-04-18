8cc4d9ac2b2c4611e8c4c12ffdca983ebec592b8
Arterixs <artemontugg@gmail.com>
2023-04-18 20:03:34 +0300

    feat: add test in search cards

--- src/pages/main/main.test.tsx
+++ src/pages/main/main.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { birdsData } from 'store/data';
 import { Main } from './Main';
 
@@ -20,5 +21,23 @@ describe('render components', () => {
     const names = screen.getAllByTestId('title_card').map((it) => it.textContent);
     const checkNames = birdsData[0].map((it) => it.name);
     expect(names).toEqual(checkNames);
+  });
+});
+
+describe('search cards', () => {
+  it('search', async () => {
+    const user = userEvent.setup();
+    render(<Main contentCard={birdsData} />);
+    const getInput = screen.getByRole('textbox');
+    await user.type(getInput, 'в');
+    const cards = screen.getAllByTestId('card');
+    expect(cards.length).toBe(2);
+    await user.type(getInput, 'О');
+    const card = screen.getAllByTestId('card');
+    expect(card.length).toBe(1);
+    const name = screen.getByTestId('title_card').textContent;
+    expect(name).toBe('Ворон');
+    await user.clear(getInput);
+    expect(screen.getAllByTestId('card').length).toBe(6);
   });
 });
